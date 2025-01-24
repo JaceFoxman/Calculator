546bad8e334013f9bdd48ace3f04cb6df5e71684
Jason Permann <JaceFoxman@gmail.com>
2025-01-24 15:50:57 -0700

    remove invalid code, add vsrible inputs 1 & 2, add choice of operators, add decison making based off user input.

--- Calculator/Calculator.vb
+++ Calculator/Calculator.vb
@@ -4,8 +4,9 @@
 'OperatorsExample
 'https://github.com/JaceFoxman/Calculator.git
 
-Option Explicit On
-Option Strict On
+Option Explicit Off
+Option Strict Off
+
 
 
 Imports System.Runtime.Remoting.Services
@@ -15,7 +16,39 @@ Module Calculator
     Sub Main()
 
 
+        Dim intVariable1 As Integer
+        Dim intVariable2 As Integer
+        Dim userInput As String
 
+        'Get variables 1 & 2 from user to use in operators
+        Console.Write("Enter variable 1: ")
+        intVariable1 = Console.ReadLine()
+        intVariable1 = CInt(intVariable1)
+        Console.WriteLine()
+
+        Console.Write("Enter variable 2: ")
+        intVariable2 = Console.ReadLine()
+        intVariable2 = CInt(intVariable2)
+        Console.WriteLine()
+
+        'Get user to chose what operator they want to use 
+        Console.WriteLine("Choose an operator: " & vbNewLine _
+                          & "1.Add " & vbNewLine _
+                          & "2.Multiply")
+
+        userInput = Console.ReadLine()
+        Console.WriteLine()
+
+        'Decison making to decided operatio based off previous user input
+        If userInput = "1" Then
+            Console.WriteLine($"Operator: {userInput}" & " Addition")
+            Console.WriteLine($"{intVariable1} + {intVariable2} = {intVariable1 + intVariable2}")
+        ElseIf userInput = "2" Then
+            Console.WriteLine($"Operator: {userInput}" & " Multiply")
+            Console.WriteLine($"{intVariable1} * {intVariable2} = {intVariable1 * intVariable2}")
+        Else
+            Console.WriteLine("Invalid Response")
+        End If
 
     End Sub
 
